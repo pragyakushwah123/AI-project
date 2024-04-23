9f359b92a9bce0d8a3495c7f2941f4f1c237b1f4
pragyakushwah123 <114136085+pragyakushwah123@users.noreply.github.com>
2024-04-23 14:26:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Faq from './component/faq/Faq';
+import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/js/bootstrap.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Faq/>
+
+      
+      
+      
     </div>
+    
   );
 }
 
